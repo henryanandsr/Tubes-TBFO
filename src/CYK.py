550884212d80c4e05cf41bbb52d8c3cf98753805
IceTeaXXD <13521024@std.stei.itb.ac.id>
2022-11-24 19:15:43 +0700

    Updated CYK and loading animation

--- src/CYK.py
+++ src/CYK.py
@@ -1,28 +1,32 @@
 import sys
-def cyk(w, R):
-    animation = "|/-\\"
-    n = len(w)
-    T = [[set([]) for j in range(n)] for i in range(n)]
-    for j in range(0, n):
-      for lhs, rule in R.items():
-        for rhs in rule:    
-          if len(rhs) == 1 and rhs[0] == w[j]:
-            T[j][j].add(lhs)
 
-      
-      for i in range(j, -1, -1):     
-        for k in range(i, j):  
-          for lhs, rule in R.items():
-            for rhs in rule:
-              if len(rhs) == 2 and rhs[0] in T[i][k] and rhs[1] in T[k + 1][j]:
-                T[i][j].add(lhs)
-                sys.stdout.write("\rLoading..." + animation[k % len(animation)])
-    
+def cyk(token,grammar):
+    animation = "|/-\\"
+    # Membuat tabel dengan ukuran token
+    tab = [[set() for i in range(len(token))] for j in range(len(token))]
+    for i in range(len(token)): # Iterasi untuk setiap token
+        for key in grammar: # Iterasi untuk setiap key pada grammar
+            for rule in grammar[key]: # Iterasi untuk setiap rule pada grammar
+                if len(rule) == 1 and rule[0] == token[i]: # Jika rule hanya memiliki 1 token dan sama dengan token
+                    tab[i][i].add(key)
+                    sys.stdout.write("\rLoading..." + animation[i % len(animation)])
+
+     # Iterasi kedua untuk setiap token
+    for j in range(len(token)):
+        for i in range(j,-1,-1): 
+            for k in range(i,j):
+                for key in grammar:
+                    for rule in grammar[key]:
+                        # Jika rule memiliki 2 token dan token pertama ada di tab[i][k] dan token kedua ada di tab[k+1][j]
+                        if len(rule) == 2 and rule[0] in tab[i][k] and rule[1] in tab[k+1][j]:
+                            tab[i][j].add(key)
+            sys.stdout.write("\rLoading..." + animation[i % len(animation)])
+            
     print()
-    if 'S' in T[0][n-1]:
+    if 'S' in tab[0][len(token)-1]:
         print("Program Successfully Compiled!!! ^V^")
     else:
         print("Program Failed to Compile T_T")
-      
+
 if __name__ == "__main__":
-  pass
+    pass
